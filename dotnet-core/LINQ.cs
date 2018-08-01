3d313736f9f4911bf77ae85e8a3e73bed32d66f6
Mauro Petrini <mpetrini@coelsa.com.ar>
2018-08-01 17:43:33 -0300

    first commit dotnet core

---
+++ dotnet-core/LINQ.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dotnet_core
+{
+    public class LINQ
+    {
+        private readonly List<string> _items = Enumerable.Range(0, 50000).Select(i => "Num" + i).ToList();
+
+        [Benchmark(Description = ".Count()")]
+        public void Count()
+        {
+            var i = _items.Count();
+        }
+
+        [Benchmark(Description = "Property Count")]
+        public void RawCount()
+        {
+            var i = _items.Count;
+        }
+    }
+}
---
+++ dotnet-core/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace dotnet_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(LINQ));
+
+            Console.Read();
+        }
+    }
+}
